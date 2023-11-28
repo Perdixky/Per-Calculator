aff68c4c8e5c160c33ba484f23187b88d28c7d8a
Perdixky <105138864+Perdixky@users.noreply.github.com>
2023-11-28 22:36:50 +0800

    删除了无用代码

--- Calculator/Calculate.h
+++ Calculator/Calculate.h
@@ -3,12 +3,3 @@
 
 double calculate();
 extern std::stringstream ss;
-
-union Token
-{
-	//默认public
-	explicit Token(const double value) :value(value) { }
-	explicit Token(const char brace) :brace(brace) { }
-	char brace{ '_' };//始终初始化，但联合类型只能有一个默认初始化
-	double value;
-};
